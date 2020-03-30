a73fe2fe6cc12adeafca330a2af1a0203ae20e4f
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-03-30 18:23:35 -0500

    Update the_better_lcs.py

--- the_better_lcs.py
+++ the_better_lcs.py
@@ -1,30 +1,20 @@
-def lcs(X, Y):
-    m = len(X) 
-    n = len(Y) 
+def lcs_length(str1, str2): 
+    str1_len = len(str1) 
+    str2_len = len(str2) 
 
-    L = [[None]*(n+1) for i in range(m+1)] 
+    L = [[0] * (str2_len + 1) for _ in range(str1_len + 1)] 
+  
+    for i in range(str1_len + 1): 
+        for j in range(str2_len + 1): 
+            L[i][j] = 0 if i==0 or j==0 else(L[i-1][j-1]+1 if str1[i-1]==str2[j-1] else max(L[i-1][j], L[i][j-1]))
 
-    for i in range(m+1): 
-        for j in range(n+1): 
-            if i == 0 or j == 0 : 
-                L[i][j] = 0
-            elif X[i-1] == Y[j-1]: 
-                L[i][j] = L[i-1][j-1]+1
-            else: 
-                L[i][j] = max(L[i-1][j] , L[i][j-1]) 
-
-    return L[m][n] 
-
-
-def test():
-
-
-        
-
-
+    return L[str1_len][str2_len] 
 
 def main():
-    pass
+    b = lcs_length('hxbecqibzpyqhosszypghkdddykjfjcajnwfmtfhqcpavqrtoipocijrmqpgzoufkjkyurczxuhkcpehbhpsuieqgjcepuhbpronmlrcgvipvibhuyqndbjbrrzpqbdegmqgjliclcgahxoouqxqpujsyfwbdthteidvigudsuoznykkzskspjufgkhaxorbrdvgodlb', 'qnnprxqpnafnhekcxljyysobbpyhynvolgtrntqtjpxpchqwgtkpxxvmwwcohxplsailheuzhkbtayvmxnttycdkbdvryjkfhshulptkuarqwuidrnjsydftsyhuueebnrjvkfvhqmyrclehcwethsqzcyfvyohzskvgttggndmdvdgollryqoswviqurrqhiqrqtyrl')
+    print(b)
+    print(len('hxbbpyhogntqppcqgkxchpsieuhbncvpuqndbjqmclchqyfttdvgoysuhrrl'))
+    
 
 
 if __name__ == "__main__":
