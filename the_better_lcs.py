dba4fa9e464037521e7c363e4bb030f2f33fd811
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-03-30 20:21:59 -0500

    Update the_better_lcs.py

--- the_better_lcs.py
+++ the_better_lcs.py
@@ -1,3 +1,27 @@
+import re
+from itertools import combinations
+
+def readFile(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    names = []
+    sequences = []
+
+    for line in lines:
+        if line == '\n': continue
+
+        l = line.rstrip()
+        
+        if re.search('^>.*$', l):
+            names.append(l)
+        else:
+            sequences.append(l)
+
+    f.close()
+
+    return (names, sequences)
+
 def lcs_length(str1, str2): 
     str1_len = len(str1) 
     str2_len = len(str2) 
@@ -11,11 +35,12 @@ def lcs_length(str1, str2):
     return L[str1_len][str2_len] 
 
 def main():
-    b = lcs_length('hxbecqibzpyqhosszypghkdddykjfjcajnwfmtfhqcpavqrtoipocijrmqpgzoufkjkyurczxuhkcpehbhpsuieqgjcepuhbpronmlrcgvipvibhuyqndbjbrrzpqbdegmqgjliclcgahxoouqxqpujsyfwbdthteidvigudsuoznykkzskspjufgkhaxorbrdvgodlb', 'qnnprxqpnafnhekcxljyysobbpyhynvolgtrntqtjpxpchqwgtkpxxvmwwcohxplsailheuzhkbtayvmxnttycdkbdvryjkfhshulptkuarqwuidrnjsydftsyhuueebnrjvkfvhqmyrclehcwethsqzcyfvyohzskvgttggndmdvdgollryqoswviqurrqhiqrqtyrl')
-    print(b)
-    print(len('hxbbpyhogntqppcqgkxchpsieuhbncvpuqndbjqmclchqyfttdvgoysuhrrl'))
+    names_and_seq = readFile('tulp3_relatives-2.txt')
+    names_and_seq = [(i, j) for i, j in zip(names_and_seq[0], names_and_seq[1])]
+
+    for i in list(combinations(names_and_seq, 2)):
+        print(i[0][0],'\t', i[1][0], ':\t', lcs_length(i[0][1], i[1][1]))
+
     
-
-
 if __name__ == "__main__":
     main()
